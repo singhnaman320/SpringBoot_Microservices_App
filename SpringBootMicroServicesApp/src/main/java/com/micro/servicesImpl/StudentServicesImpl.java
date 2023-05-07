4a6f5c9c729e2eeece19d8c2fb848a3e902f583d
naman kumar singh <singhnaman320@gmail.com>
2023-05-07 17:17:02 +0530

    Adding Service Impl layer

---
+++ SpringBootMicroServicesApp/src/main/java/com/micro/servicesImpl/StudentServicesImpl.java
@@ -0,0 +1,41 @@
+package com.micro.servicesImpl;
+
+import java.util.List;
+
+import com.micro.entities.Student;
+import com.micro.exceptions.StudentNotFoundException;
+import com.micro.services.StudentServices;
+
+public class StudentServicesImpl implements StudentServices{
+
+	@Override
+	public Student registerStudent(Student student) throws StudentNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> getAllStudents() throws StudentNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student updateStudent(Student student) throws StudentNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student deleteStudentByRoll(Integer roll) throws StudentNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> getStudentByName(String name) throws StudentNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
