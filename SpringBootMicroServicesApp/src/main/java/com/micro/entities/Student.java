6e2098ec5cdb9f132b538f7312bad09a504b30b7
naman kumar singh <singhnaman320@gmail.com>
2023-05-07 16:49:58 +0530

    adding student entity

--- SpringBootMicroServicesApp/src/main/java/com/micro/entities/Student.java
+++ SpringBootMicroServicesApp/src/main/java/com/micro/entities/Student.java
@@ -1,5 +1,17 @@
 package com.micro.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer roll;
+	private String name;
+	private String address;
+	private Integer marks;
 }
