1dc4acdd918136928a0dcd2a59cfb85fb84fbb61
naman kumar singh <singhnaman320@gmail.com>
2023-05-07 17:02:15 +0530

    Adding StudentNotFoundException

---
+++ SpringBootMicroServicesApp/src/main/java/com/micro/exceptions/ProductNotFoundException.java
@@ -0,0 +1,13 @@
+package com.micro.exceptions;
+
+public class ProductNotFoundException extends Exception{
+
+	public ProductNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ProductNotFoundException(String message) {
+		
+		super(message);
+	}
+}
