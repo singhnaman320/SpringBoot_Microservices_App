85fa2ed34552367b0ac80b8e4815554cfe28d549
naman kumar singh <singhnaman320@gmail.com>
2023-05-07 16:51:50 +0530

    adding student entity

--- SpringBootMicroServicesApp/src/main/java/com/micro/entities/Student.java
+++ SpringBootMicroServicesApp/src/main/java/com/micro/entities/Student.java
@@ -31,6 +31,37 @@ public class Student {
 	public String toString() {
 		return "Student [roll=" + roll + ", name=" + name + ", address=" + address + ", marks=" + marks + "]";
 	}
-	
+
+	public Integer getRoll() {
+		return roll;
+	}
+
+	public void setRoll(Integer roll) {
+		this.roll = roll;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getMarks() {
+		return marks;
+	}
+
+	public void setMarks(Integer marks) {
+		this.marks = marks;
+	}
 	
 }
