65711fd2d8566dd7caeaa552da88e03244da4ced
naman kumar singh <singhnaman320@gmail.com>
2023-05-07 18:39:46 +0530

    Updating entity class

--- SpringBootMicroServicesApp/src/main/java/com/micro/entities/Student.java
+++ SpringBootMicroServicesApp/src/main/java/com/micro/entities/Student.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 public class Student {
@@ -23,6 +24,14 @@ public class Student {
 	@NotNull(message = "Student marks cannot be null")
 	@Min(value = 0, message = "Student marks can not be negative")
 	private Integer marks;
+	
+	@NotNull(message = "Student courseId cannot be null")
+	@Min(value = 0, message = "Student courseId can not be zero or negative")
+	private Integer courseId;
+	
+	@NotNull(message = "Student mobile number cannot be null")
+	@Pattern(regexp = "[7986]{1}[0-9]{9}", message = "Enter valid 10 digit mobile number")
+	private String mobileNumber;
 	
 	public Student() {
 		super();
