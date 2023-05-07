a42c79ad7fafc78d523a18cc06537f0aa849278c
naman kumar singh <singhnaman320@gmail.com>
2023-05-07 17:15:31 +0530

    Adding Service layer

---
+++ SpringBootMicroServicesApp/src/main/java/com/micro/services/StudentServices.java
@@ -0,0 +1,15 @@
+package com.micro.services;
+
+import java.util.List;
+
+import com.micro.entities.Student;
+import com.micro.exceptions.StudentNotFoundException;
+
+public interface StudentServices {
+
+	public Student registerStudent(Student student)throws StudentNotFoundException;
+	public List<Student> getAllStudents()throws StudentNotFoundException;
+	public Student updateStudent(Student student)throws StudentNotFoundException;
+	public Student deleteStudentByRoll(Integer roll)throws StudentNotFoundException;
+	public List<Student> getStudentByName(String name)throws StudentNotFoundException;
+}
