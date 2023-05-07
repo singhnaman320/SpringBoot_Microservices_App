ad9513d0b8186e0bc6e59f1f2dd73ef4ee2dd8e5
naman kumar singh <singhnaman320@gmail.com>
2023-05-07 18:43:17 +0530

    Updating entity class

--- SpringBootMicroServicesApp/src/main/java/com/micro/entities/Student.java
+++ SpringBootMicroServicesApp/src/main/java/com/micro/entities/Student.java
@@ -36,6 +36,86 @@ public class Student {
 	public Student() {
 		super();
 	}
-	
+
+	public Student(Integer studentId, @NotNull(message = "Student name cannot be null") String name,
+			@NotNull(message = "Student address cannot be null") String address,
+			@NotNull(message = "Student marks cannot be null") @Min(value = 0, message = "Student marks can not be negative") Integer marks,
+			@NotNull(message = "Student courseId cannot be null") @Min(value = 0, message = "Student courseId can not be zero or negative") Integer courseId,
+			@NotNull(message = "Student mobile number cannot be null") @Pattern(regexp = "[7986]{1}[0-9]{9}", message = "Enter valid 10 digit mobile number") String mobileNumber) {
+		super();
+		this.studentId = studentId;
+		this.name = name;
+		this.address = address;
+		this.marks = marks;
+		this.courseId = courseId;
+		this.mobileNumber = mobileNumber;
+	}
+
+	public Student(@NotNull(message = "Student name cannot be null") String name,
+			@NotNull(message = "Student address cannot be null") String address,
+			@NotNull(message = "Student marks cannot be null") @Min(value = 0, message = "Student marks can not be negative") Integer marks,
+			@NotNull(message = "Student courseId cannot be null") @Min(value = 0, message = "Student courseId can not be zero or negative") Integer courseId,
+			@NotNull(message = "Student mobile number cannot be null") @Pattern(regexp = "[7986]{1}[0-9]{9}", message = "Enter valid 10 digit mobile number") String mobileNumber) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.marks = marks;
+		this.courseId = courseId;
+		this.mobileNumber = mobileNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", name=" + name + ", address=" + address + ", marks=" + marks
+				+ ", courseId=" + courseId + ", mobileNumber=" + mobileNumber + "]";
+	}
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getMarks() {
+		return marks;
+	}
+
+	public void setMarks(Integer marks) {
+		this.marks = marks;
+	}
+
+	public Integer getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Integer courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
 	
 }
