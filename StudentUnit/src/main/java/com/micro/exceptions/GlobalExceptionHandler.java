78c36f567c4a79ea62e57dbc4843743e24239883
naman kumar singh <singhnaman320@gmail.com>
2023-05-08 19:08:36 +0530

    Adding Global exception handler

--- StudentUnit/src/main/java/com/micro/exceptions/GlobalExceptionHandler.java
+++ StudentUnit/src/main/java/com/micro/exceptions/GlobalExceptionHandler.java
@@ -14,45 +14,45 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 public class GlobalExceptionHandler {
 
 	// For Customizing our error body as a response if any error occurs then we make little changes
-		// WebRequest req--->To print Description
+	// WebRequest req--->To print Description
 		
-		@ExceptionHandler(StudentNotFoundException.class)
-		public ResponseEntity<CustomizeError> exceptionHandler(StudentNotFoundException bnf, WebRequest req){
+	@ExceptionHandler(StudentNotFoundException.class)
+	public ResponseEntity<CustomizeError> exceptionHandler(StudentNotFoundException bnf, WebRequest req){
 			
-			CustomizeError customizeErr= new CustomizeError(LocalDateTime.now(), bnf.getMessage(), req.getDescription(false));
+		CustomizeError customizeErr= new CustomizeError(LocalDateTime.now(), bnf.getMessage(), req.getDescription(false));
 			
-			return new ResponseEntity<CustomizeError>(customizeErr, HttpStatus.BAD_REQUEST);
-		}
+		return new ResponseEntity<CustomizeError>(customizeErr, HttpStatus.BAD_REQUEST);
+	}
 		
-		// For exception if no uri found with given name--> some properties also added to application.properties
+	// For exception if no uri found with given name--> some properties also added to application.properties
 		
-		@ExceptionHandler(NoHandlerFoundException.class)
-		public ResponseEntity<CustomizeError> notFoundUri(NoHandlerFoundException nhf, WebRequest req){
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<CustomizeError> notFoundUri(NoHandlerFoundException nhf, WebRequest req){
 			
-			CustomizeError customizeErr= new CustomizeError(LocalDateTime.now(), nhf.getMessage(), req.getDescription(false));
+		CustomizeError customizeErr= new CustomizeError(LocalDateTime.now(), nhf.getMessage(), req.getDescription(false));
 			
-			return new ResponseEntity<CustomizeError>(customizeErr, HttpStatus.BAD_REQUEST);
-		}
+		return new ResponseEntity<CustomizeError>(customizeErr, HttpStatus.BAD_REQUEST);
+	}
 		
 		
-		//To handle any generic type of exception, if occurs
+	//To handle any generic type of exception, if occurs
 		
-		@ExceptionHandler(Exception.class)
-		public ResponseEntity<CustomizeError> anyGenericException(Exception e, WebRequest req){
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<CustomizeError> anyGenericException(Exception e, WebRequest req){
 			
-			CustomizeError customizeErr= new CustomizeError(LocalDateTime.now(), e.getMessage(), req.getDescription(false));
+		CustomizeError customizeErr= new CustomizeError(LocalDateTime.now(), e.getMessage(), req.getDescription(false));
 			
-			return new ResponseEntity<CustomizeError>(customizeErr, HttpStatus.BAD_REQUEST);
-		}
+		return new ResponseEntity<CustomizeError>(customizeErr, HttpStatus.BAD_REQUEST);
+	}
 		
-		// Handling the validation exception
+	// Handling the validation exception
 		
-		@ExceptionHandler(MethodArgumentNotValidException.class)
-		public ResponseEntity<CustomizeError> validationErr(MethodArgumentNotValidException me){
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<CustomizeError> validationErr(MethodArgumentNotValidException me){
 			
-			CustomizeError customizeErr= new CustomizeError(LocalDateTime.now(), "Validation Error", me.getBindingResult().getFieldError().getDefaultMessage());
+		CustomizeError customizeErr= new CustomizeError(LocalDateTime.now(), "Validation Error", me.getBindingResult().getFieldError().getDefaultMessage());
 			
-			return new ResponseEntity<CustomizeError>(customizeErr, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<CustomizeError>(customizeErr, HttpStatus.BAD_REQUEST);
 
-		}
+	}
 }
