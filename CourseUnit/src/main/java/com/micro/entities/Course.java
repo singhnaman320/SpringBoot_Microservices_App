7d1d2fe3d4818cea6b0bc5af58675faeaf2272e2
naman kumar singh <singhnaman320@gmail.com>
2023-05-08 18:36:19 +0530

    Adding course entity

--- CourseUnit/src/main/java/com/micro/entities/Course.java
+++ CourseUnit/src/main/java/com/micro/entities/Course.java
@@ -4,11 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Course {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer cId;
+	private Integer courseId;
+	
+	@NotNull(message = "Course name cannot be null")
+	private String courseName;
+	
+	
 }
