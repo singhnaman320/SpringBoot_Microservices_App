a4ca6dfb9c9d0770ed2484124f1dfed2ead236b6
naman kumar singh <singhnaman320@gmail.com>
2023-05-08 19:38:34 +0530

    Adding service layer

--- CourseUnit/src/main/java/com/micro/services/CourseService.java
+++ CourseUnit/src/main/java/com/micro/services/CourseService.java
@@ -1,5 +1,21 @@
 package com.micro.services;
 
+import java.util.List;
+
+import com.micro.entities.Course;
+import com.micro.exceptions.CourseNotFoundException;
+
 public interface CourseService {
 
+	public Course registerCourse(Course course)throws CourseNotFoundException;
+	
+	public List<Course> getAllCourses()throws CourseNotFoundException;
+	
+	public Course updateCourse(Course course)throws CourseNotFoundException;
+	
+	public Course getCourseById(Integer courseId)throws CourseNotFoundException;
+	
+	public Course deleteCourseById(Integer courseId)throws CourseNotFoundException;
+	
+	public List<Course> getCourseByName(String name)throws CourseNotFoundException;
 }
