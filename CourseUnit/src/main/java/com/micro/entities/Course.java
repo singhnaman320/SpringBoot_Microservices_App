07df8c792794545b75ddb06a097a50210d157906
naman kumar singh <singhnaman320@gmail.com>
2023-05-08 18:23:01 +0530

    Updating course unit with database properties

---
+++ CourseUnit/src/main/java/com/micro/entities/Course.java
@@ -0,0 +1,14 @@
+package com.micro.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer cId;
+}
