7ca4434b24927e5a1b7e5cc04a4a74abb38a8ff3
naman kumar singh <singhnaman320@gmail.com>
2023-05-08 22:51:24 +0530

    Adding controller layer

--- CourseUnit/src/main/java/com/micro/controllers/CourseController.java
+++ CourseUnit/src/main/java/com/micro/controllers/CourseController.java
@@ -1,8 +1,13 @@
 package com.micro.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.micro.services.CourseService;
 
 @RestController
 public class CourseController {
 
+	@Autowired
+	private CourseService courseService;
 }
