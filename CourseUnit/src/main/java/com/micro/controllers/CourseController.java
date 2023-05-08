48a64a1e86d7075fd6b4ab89cf457e04bd9fb931
naman kumar singh <singhnaman320@gmail.com>
2023-05-08 23:12:58 +0530

    Adding controller layer

--- CourseUnit/src/main/java/com/micro/controllers/CourseController.java
+++ CourseUnit/src/main/java/com/micro/controllers/CourseController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -56,5 +58,13 @@ public class CourseController {
 		return new ResponseEntity<Course>(updateCourseDetails, HttpStatus.ACCEPTED);
 	}
 	
+	// 4. Delete Student by its Id
 	
+	@DeleteMapping("/courses/{Id}")
+	public ResponseEntity<Course> deleteCourseByIdHandler(@PathVariable("Id") Integer courseId) throws CourseNotFoundException{
+			
+		Course deleteCourse = courseService.deleteCourseById(courseId);
+			
+		return  new ResponseEntity<Course>(deleteCourse, HttpStatus.ACCEPTED);
+	}
 }
