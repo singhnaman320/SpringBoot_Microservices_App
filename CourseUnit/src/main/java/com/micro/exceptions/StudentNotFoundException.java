f5b6476996b784beb56cd193cda7373c48077d6b
naman kumar singh <singhnaman320@gmail.com>
2023-05-08 19:02:32 +0530

    Adding CourseNotFoundException

---
+++ CourseUnit/src/main/java/com/micro/exceptions/StudentNotFoundException.java
@@ -0,0 +1,13 @@
+package com.micro.exceptions;
+
+public class StudentNotFoundException extends Exception{
+
+	public StudentNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public StudentNotFoundException(String message) {
+		
+		super(message);
+	}
+}
