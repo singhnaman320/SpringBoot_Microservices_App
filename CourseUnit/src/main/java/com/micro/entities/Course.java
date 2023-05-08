db00ea80cbb96cf4f72ad91637c94c83321fd649
naman kumar singh <singhnaman320@gmail.com>
2023-05-08 18:40:06 +0530

    Adding course entity

--- CourseUnit/src/main/java/com/micro/entities/Course.java
+++ CourseUnit/src/main/java/com/micro/entities/Course.java
@@ -45,6 +45,29 @@ public class Course {
 		return "Course [courseId=" + courseId + ", courseName=" + courseName + ", courseDuration=" + courseDuration
 				+ "]";
 	}
-	
+
+	public Integer getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Integer courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public Integer getCourseDuration() {
+		return courseDuration;
+	}
+
+	public void setCourseDuration(Integer courseDuration) {
+		this.courseDuration = courseDuration;
+	}
 	
 }
