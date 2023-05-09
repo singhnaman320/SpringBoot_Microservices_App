1f97030d84d2d8f1974b34ddc963c1e91850b5cb
naman kumar singh <singhnaman320@gmail.com>
2023-05-09 15:56:13 +0530

    Modifying applicatication.properties and Course controller

--- CourseUnit/src/main/java/com/micro/controllers/CourseController.java
+++ CourseUnit/src/main/java/com/micro/controllers/CourseController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.micro.entities.Course;
@@ -20,6 +21,7 @@ import com.micro.services.CourseService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/courseDetails")
 public class CourseController {
 
 	@Autowired
