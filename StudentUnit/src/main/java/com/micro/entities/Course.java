9fb078f67f54f6ac84421ccd13f9d56e13c7ee81
naman kumar singh <singhnaman320@gmail.com>
2023-05-10 16:18:35 +0530

    Adding StudentDetails and Course

---
+++ StudentUnit/src/main/java/com/micro/entities/Course.java
@@ -0,0 +1,8 @@
+package com.micro.entities;
+
+public class Course {
+
+	private Integer courseId;          
+	private String courseName;      
+	private Integer courseDuration;
+}
--- StudentUnit/src/main/java/com/micro/entities/StudentDetails.java
+++ StudentUnit/src/main/java/com/micro/entities/StudentDetails.java
@@ -2,4 +2,10 @@ package com.micro.entities;
 
 public class StudentDetails {
 
+	private Integer studentId;
+	private String studentName;
+	private String mobileNumber;
+	private Integer courseId;
+	private Course course;
+	
 }
