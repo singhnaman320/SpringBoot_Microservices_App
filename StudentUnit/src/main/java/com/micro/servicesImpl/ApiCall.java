ac8047a18150426b041eb694fb8312997cc019a4
naman kumar singh <singhnaman320@gmail.com>
2023-05-10 17:32:22 +0530

    Adding Service Impl and Api Call

--- StudentUnit/src/main/java/com/micro/servicesImpl/ApiCall.java
+++ StudentUnit/src/main/java/com/micro/servicesImpl/ApiCall.java
@@ -1,5 +1,24 @@
 package com.micro.servicesImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.micro.entities.Course;
+
+@Service
 public class ApiCall {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private static final String courseMicroserviceBaseURL = "http://course-service";
+	
+	public Course getCourseDetails(Integer courseId) {
+		
+		Course course = restTemplate.getForObject(courseMicroserviceBaseURL + "/courseDetails/getcourses/{courseId}", 
+				Course.class, courseId);
+		
+		return course;
+	}
 }
