dc39cbcbdb9b94e2cf8d3cbc940bc3d619184fd9
naman kumar singh <singhnaman320@gmail.com>
2023-05-10 16:21:22 +0530

    Adding StudentDetails and Course

--- StudentUnit/src/main/java/com/micro/entities/Course.java
+++ StudentUnit/src/main/java/com/micro/entities/Course.java
@@ -1,8 +1,57 @@
 package com.micro.entities;
 
+//It is created so that we can access another microservice course from student microservice
 public class Course {
 
 	private Integer courseId;          
 	private String courseName;      
 	private Integer courseDuration;
+	
+	public Course() {
+		super();
+	}
+
+	public Course(Integer courseId, String courseName, Integer courseDuration) {
+		super();
+		this.courseId = courseId;
+		this.courseName = courseName;
+		this.courseDuration = courseDuration;
+	}
+
+	public Course(String courseName, Integer courseDuration) {
+		super();
+		this.courseName = courseName;
+		this.courseDuration = courseDuration;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [courseId=" + courseId + ", courseName=" + courseName + ", courseDuration=" + courseDuration
+				+ "]";
+	}
+
+	public Integer getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Integer courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public Integer getCourseDuration() {
+		return courseDuration;
+	}
+
+	public void setCourseDuration(Integer courseDuration) {
+		this.courseDuration = courseDuration;
+	}
+	
 }
--- StudentUnit/src/main/java/com/micro/entities/StudentDetails.java
+++ StudentUnit/src/main/java/com/micro/entities/StudentDetails.java
@@ -1,5 +1,6 @@
 package com.micro.entities;
 
+// It is created so that we can access another microservice course from student microservice
 public class StudentDetails {
 
 	private Integer studentId;
@@ -8,4 +9,70 @@ public class StudentDetails {
 	private Integer courseId;
 	private Course course;
 	
+	public StudentDetails() {
+		super();
+	}
+
+	public StudentDetails(Integer studentId, String studentName, String mobileNumber, Integer courseId, Course course) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.mobileNumber = mobileNumber;
+		this.courseId = courseId;
+		this.course = course;
+	}
+
+	public StudentDetails(String studentName, String mobileNumber, Integer courseId, Course course) {
+		super();
+		this.studentName = studentName;
+		this.mobileNumber = mobileNumber;
+		this.courseId = courseId;
+		this.course = course;
+	}
+
+	@Override
+	public String toString() {
+		return "StudentDetails [studentId=" + studentId + ", studentName=" + studentName + ", mobileNumber="
+				+ mobileNumber + ", courseId=" + courseId + ", course=" + course + "]";
+	}
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public Integer getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Integer courseId) {
+		this.courseId = courseId;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
 }
