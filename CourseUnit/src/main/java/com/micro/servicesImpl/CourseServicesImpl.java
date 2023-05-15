03668251508c1a32bd1e08b3fa8a334bad646c4a
naman kumar singh <singhnaman320@gmail.com>
2023-05-15 16:53:59 +0530

    Modifying Course/pom.xml

--- CourseUnit/src/main/java/com/micro/servicesImpl/CourseServicesImpl.java
+++ CourseUnit/src/main/java/com/micro/servicesImpl/CourseServicesImpl.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 
@@ -85,6 +86,22 @@ public class CourseServicesImpl implements CourseService{
 	public Course getCourseById(Integer courseId) throws CourseNotFoundException {
 		// TODO Auto-generated method stub
 		
+		logger.info("logs before custom span");
+		Span newSpan = this.tracer.nextSpan().name("custom-log");
+		
+		try (Tracer.SpanInScope ws = this.tracer.withSpan(newSpan.start())) {
+		    // ...
+		    // You can tag a span
+		    newSpan.tag("custom-tag", "##333##");
+		    // ...
+		    logger.info("Logs in custom span");
+		}
+		finally {
+		    // Once done remember to end the span. This will allow collecting
+		    // the span to send it to a distributed tracing system e.g. Zipkin
+		    newSpan.end();
+		}
+
 		return dao.findById(courseId)
 				.orElseThrow(()-> new CourseNotFoundException("Unable to get course details with given Id "+ courseId));
 	}
