50d4294273e0d458da83bf573dc720e715b48f23
naman kumar singh <singhnaman320@gmail.com>
2023-05-18 18:26:13 +0530

    Modifying StudentUnit/controller

--- StudentUnit/src/main/java/com/micro/controllers/StudentController.java
+++ StudentUnit/src/main/java/com/micro/controllers/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -32,6 +33,9 @@ public class StudentController {
 
 	@Autowired
 	private StudentServices studentService;
+	
+	@Value("${message:default message}")
+	private String message;
 	
 	private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
 	
