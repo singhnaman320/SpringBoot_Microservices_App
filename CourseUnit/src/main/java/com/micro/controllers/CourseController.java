b77de162bf178d16ea5e1ec13d913b9e3a89deb5
naman kumar singh <singhnaman320@gmail.com>
2023-05-20 20:13:21 +0530

    Modifying course/courseController

--- CourseUnit/src/main/java/com/micro/controllers/CourseController.java
+++ CourseUnit/src/main/java/com/micro/controllers/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -25,6 +26,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/courseDetails")
+@RefreshScope
 public class CourseController {
 
 	@Autowired
