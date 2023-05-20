7a67e26230933469bacf061b35635431997f25a7
naman kumar singh <singhnaman320@gmail.com>
2023-05-20 17:37:20 +0530

    Modifying courseUnit/application.properties

--- CourseUnit/src/main/java/com/micro/controllers/CourseController.java
+++ CourseUnit/src/main/java/com/micro/controllers/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,6 +29,8 @@ public class CourseController {
 
 	@Autowired
 	private CourseService courseService;
+	
+	
 	
 	// 1. Registering Courses
 	
